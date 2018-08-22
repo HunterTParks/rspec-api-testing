72b8c3760bf8699fbb01be83469715f7e09e751a
Hunter <hunter.thomas.parks@gmail.com>
2018-08-21 23:29:03 -0700

    add specs for title and text validations

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe Todo, :type => :model do
     it "is valid with valid attributes" do
       expect(Todo.new).to be_valid
     end
+
+    it "is not valid without a title" do
+      item = Todo.new(title: nil)
+      expect(item).to be_valid
+    end
+
+    it "is not valid without a text body" do
+      item = Todo.new(text: nil)
+      expect(item).to be_valid
+    end
   end
-  it "is not valid without a title"
-  it "is not valid without a text body"
 end
