77ebeac35a1cbdd8361d683356cc2c84225a3ff8
Hunter <hunter.thomas.parks@gmail.com>
2018-08-22 00:01:48 -0700

    add controller specs to TodoController

---
+++ spec/controllers/todo_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe TodoController, :type => :controller do
+  context "GET /index" do
+    before do
+      get :index
+    end
+
+    it "will return a list of items" do
+      item = Todo.new(title: "Clean the dishes", text: "Complete by 5 PM")
+      response.body.should contain(item)
+    end
+  end
+end
