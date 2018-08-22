0f01bb875fa5fcdabd4983a9eb16b9e08749173d
Hunter <hunter.thomas.parks@gmail.com>
2018-08-21 23:25:01 -0700

    add context to organize specs

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Todo, :type => :model do
-  it "is valid with valid attributes"
+  context "When a new Todo item is created it..." do
+    it "is valid with valid attributes" do
+      expect(Todo.new).to be_valid
+    end
+  end
   it "is not valid without a title"
   it "is not valid without a text body"
 end
