8ee7a05b27e074c16bd4e399758eb39fdefb26ec
Hunter <hunter.thomas.parks@gmail.com>
2018-08-21 23:19:07 -0700

    add specs for Model todo

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Todo, :type => :model do
   it "is valid with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without a text body"
 end
