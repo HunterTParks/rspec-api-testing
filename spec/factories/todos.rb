b7c55ff6e98b2d70ad1b8a7435521c79f6961ec2
Hunter <hunter.thomas.parks@gmail.com>
2018-08-23 20:25:04 -0700

    edit factory to remove deprecated functionality

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :todo do |f|
-    f.title "Do the dishes"
-    f.text "Complete by 5 PM"
+    f.title { "Do the dishes" }
+    f.text { "Complete by 5 PM" }
   end
 end
