7015b8acf4bd34fcb32387b78b17ed9b5f6f3873
Hunter <hunter.thomas.parks@gmail.com>
2018-08-24 13:58:29 -0700

    add specs for getting a response and getting list

--- spec/api/todo_request_spec.rb
+++ spec/api/todo_request_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "todo", :type => :request do
-  
+  context "Get todo#Index" do
+    it "returns a successful response"
+    it "returns a list of Todos"
+  end
 end
