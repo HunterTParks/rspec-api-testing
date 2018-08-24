6f73336024877fae7dbf99a197392d9bd1fb1fde
Hunter <hunter.thomas.parks@gmail.com>
2018-08-24 13:59:08 -0700

    add before that gets '/index' to specs

--- spec/api/todo_request_spec.rb
+++ spec/api/todo_request_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "todo", :type => :request do
   context "Get todo#Index" do
+    before do
+      get "/index"
+    end
     it "returns a successful response"
     it "returns a list of Todos"
   end
