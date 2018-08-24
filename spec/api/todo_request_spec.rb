dcda799339239b98fa93125d40686bb42efcdea8
Hunter <hunter.thomas.parks@gmail.com>
2018-08-24 16:49:58 -0700

    add empty specs for user's requesting 1 todo item

--- spec/api/todo_request_spec.rb
+++ spec/api/todo_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Get/edit/delete todos", :type => :request do
   let!(:todos) { FactoryBot.create_list(:todo, 20)}
 
-  context "Get todo#Index" do
+  context "When a user requests todo#Index it..." do
     before do
       get "/todo"
     end
@@ -13,5 +13,10 @@ RSpec.describe "Get/edit/delete todos", :type => :request do
     it "returns a list of Todos" do
       expect(JSON.parse(response.body).size).to eq(20)
     end
+  end
+
+  context "When a user requests todo#Show it..." do
+    it "returns a successful response"
+    it "returns one todo item"
   end
 end
