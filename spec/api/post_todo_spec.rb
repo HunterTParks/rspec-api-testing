79712e071f4e73177e9715d2d76d167f696acb3f
Hunter <hunter.thomas.parks@gmail.com>
2018-08-27 22:22:22 -0700

    add specs for Todo POST request
    These specs are for when a user sends a POST request and they will
    verify the entries.

--- spec/api/post_todo_spec.rb
+++ spec/api/post_todo_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe 'Post todo routes', :type => :request do
   before do
     post "/todo", params: { :title => 'Star Wars', :text => 'This is a texting text for Star wars'}
   end
+
+  context "When a user submits a request it..." do
+    it "returns an http response (200)"
+    it "returns the title of Todo item"
+    it "returns the text of Todo item"
+  end
 end
