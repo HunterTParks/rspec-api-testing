7370c61825e0d8a12b55f5ab81c38bcb7bbd98f3
Hunter <hunter.thomas.parks@gmail.com>
2018-08-27 22:14:43 -0700

    add parameters to POST request

--- spec/api/post_todo_spec.rb
+++ spec/api/post_todo_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Post todo routes', :type => :request do
   before do
-    post "/todo"
+    post "/todo", params: { :title => 'Star Wars', :text => 'This is a texting text for Star wars'}
   end
 end
