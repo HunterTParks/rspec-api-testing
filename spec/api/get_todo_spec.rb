f7c7ec1ca7d1cd29ab72eeeb81eddade104c37f1
Hunter <hunter.thomas.parks@gmail.com>
2018-08-29 22:01:19 -0700

    move get requests into context
    The reason I did this is because I want to test the edge cases of
    getting a list of Todos. Such as when there isn't a todo object in the
    database or (eventually) the todo is not owned by you. I do not think
    this is standard coding practice but HEY! If it works, don't fix it.
    
    I'm 100% certain a professional would be very angry at this commit.

--- spec/api/get_todo_spec.rb
+++ spec/api/get_todo_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Get all todos route", :type => :request do
-  let!(:todos) { FactoryBot.create_list(:todo, 20)}
-  before do
-    get "/todo"
-  end
-
   context "When a resource is found it..." do
+    let!(:todos) { FactoryBot.create_list(:todo, 20)}
+
     it "returns an http response (200)" do
+      get "/todo"
       expect(response).to have_http_status(200)
     end
     it "returns a list of Todos" do
+      get "/todo"
       expect(JSON.parse(response.body).size).to eq(20)
     end
   end
