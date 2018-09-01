5dd0e4b3bd4ecde1e08ab24e71f500c0020fd5ab
Hunter <hunter.thomas.parks@gmail.com>
2018-09-01 11:17:49 -0700

    add tests that check for http response status

--- spec/api/delete_todo_spec.rb
+++ spec/api/delete_todo_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "DELETE Todo items", :type => :request do
+  before do
+    FactoryBot.create(:todo)
+    delete "/todo/1"
+  end
   context "When a user submits a request it...." do
-    it "returns an http response (204)"
+    it "returns an http response (204)" do
+      expect(response).to have_http_status(204)
+    end
   end
 
   context "When a request is invalid" do
