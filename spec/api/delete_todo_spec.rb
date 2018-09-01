daeb508dd6f88c78b5bc38a5811e747e3ba38de2
Hunter <hunter.thomas.parks@gmail.com>
2018-08-31 17:46:20 -0700

    add specs for DELETE requests of model Todo
    I have added specs that expect different http responses based on
    different conditions. One spec is expecting a valid error message.

---
+++ spec/api/delete_todo_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "DELETE Todo items", :type => :request do
+  context "When a user submits a request it...." do
+    it "returns an http response (204)"
+  end
+
+  context "When a request is invalid" do
+    it "returns an http response (400)"
+    it "returns a valid error message"
+  end
+end
