90ce814c3262dc41f095109b826430a763adcd5b
Hunter <hunter.thomas.parks@gmail.com>
2018-09-01 12:06:01 -0700

    move destroy request into test
    The reason for this move is because I need to make two different
    requests for tests I have made. The second request is to make sure the
    user cannot destroy something in the database that doesn't exist.

--- spec/api/delete_todo_spec.rb
+++ spec/api/delete_todo_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "DELETE Todo items", :type => :request do
-  before do
-    FactoryBot.create(:todo)
-    delete "/todo/1"
-  end
   context "When a user submits a request it...." do
     it "returns an http response (204)" do
+      FactoryBot.create(:todo)
+      delete "/todo/1"
       expect(response).to have_http_status(204)
     end
   end
