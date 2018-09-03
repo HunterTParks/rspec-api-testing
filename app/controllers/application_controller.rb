94aff153349b7b74bfa77f22d107554b0007d84f
Hunter <hunter.thomas.parks@gmail.com>
2018-09-03 15:18:31 -0700

    add 'before_action' to application controller
    This commit makes the user have to be logged in in order to use the
    application. This commit CAUSES some tests to fail that will be fixed
    later.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Response
   include ExceptionHandler
+
+  before_action :authenticate_user!
 end
