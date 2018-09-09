179dc5b38ee19f72ca1f4342d7dab3e81e78f03a
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 12:44:22 -0700

    edit factory to use correct syntax to avoid warnings

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :group do
-    name "MyString"
-    description "MyString"
+  factory :group do |f|
+    f.name { "User Group" }
+    f.description { "This is the description of the Group" }
   end
 end
