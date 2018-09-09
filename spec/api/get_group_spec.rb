82d7f5bf3d4ccfbdcb4456e93daaf8f7e61d21c8
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 16:33:47 -0700

    edit 'Rspec' to 'RSpec' to stop throwing exceptions

--- spec/api/get_group_spec.rb
+++ spec/api/get_group_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe "Get all group routes", type: :request do
+RSpec.describe "Get all group routes", type: :request do
   context "When a resource is found it..." do
     it "should return an http response (200)" do
       expect(response).to have_http_status(200)
