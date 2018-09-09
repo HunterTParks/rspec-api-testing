c1de53a9ec261424ee39369de24b9b7fa809326e
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 11:20:56 -0700

    add authentication to pass GET Request tests
    I included create and login helpers into spec file, created a seed user,
    called the login method for each test, moved the before statement into
    the context, and passed the authentication token from the login POST
    request and passed them into the "/todo" GET request as headers. This is
    all for passing tests.

--- spec/api/get_todo_spec.rb
+++ spec/api/get_todo_spec.rb
@@ -1,22 +1,29 @@
 require 'rails_helper'
+require 'login_helper'
+require 'create_user_helper'
 
 RSpec.describe "Get all todos route", :type => :request do
   context "When a resource is found it..." do
-    let!(:todos) { FactoryBot.create_list(:todo, 20)}
-
+    before(:each) do
+      # Create users and then skip the email confirmation before
+      # saving into the database. 'create_user' is from the
+      # helper 'create_user_helper'
+      create_user
+      FactoryBot.create_list(:todo, 20)
+      login
+      auth_params = get_auth_params(response)
+      get "/todo", headers: auth_params
+    end
     it "returns an http response (200)" do
-      get "/todo"
       expect(response).to have_http_status(200)
     end
     it "returns a list of Todos" do
-      get "/todo"
       expect(JSON.parse(response.body).size).to eq(20)
     end
   end
 
   context "When a resource is not found it..." do
     it "returns an http response (204)" do
-      get "/todo"
       expect(response).to have_http_status(204)
     end
   end
