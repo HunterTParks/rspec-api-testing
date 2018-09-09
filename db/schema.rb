89c1700bc52cdd4b0097a855b3acd2333a279517
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 12:39:09 -0700

    migrate group model into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_045313) do
+ActiveRecord::Schema.define(version: 2018_09_09_193443) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
