61f3793b3e01e6e8a623cb1645bb7abd740996dd
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 16:32:09 -0700

    add tests expecting http responses 200 and 204

--- spec/api/get_group_spec.rb
+++ spec/api/get_group_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 Rspec.describe "Get all group routes", type: :request do
   context "When a resource is found it..." do
-    it "should return an http response (200)"
+    it "should return an http response (200)" do
+      expect(response).to have_http_status(200)
+    end
   end
 
   context "When a resource is not found it..." do
-    it "should return an http response (204)"
+    it "should return an http response (204)" do
+      expect(response).to have_http_status(204)
+    end
   end
 end
