4bbe829c023c87bf555873f7d988fc92bd0976bd
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 16:01:03 -0700

    add group_id to Todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_09_202202) do
+ActiveRecord::Schema.define(version: 2018_09_09_225653) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_09_202202) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id"
   end
 
   create_table "users", force: :cascade do |t|
