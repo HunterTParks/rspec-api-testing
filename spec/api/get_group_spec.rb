92ddd181c93a1d474653a140babb9643323ebcd3
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 16:30:27 -0700

    add specs expecting http responses

---
+++ spec/api/get_group_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+Rspec.describe "Get all group routes", type: :request do
+  context "When a resource is found it..." do
+    it "should return an http response (200)"
+  end
+
+  context "When a resource is not found it..." do
+    it "should return an http response (204)"
+  end
+end
