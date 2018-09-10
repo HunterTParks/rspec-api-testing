3428a36eb68cbd5d821ac2702ff9285743797736
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 17:51:30 -0700

    add succesful tests for getting and creating groups
    This commit successfully passes tests when finding, successfully or not, groups in the database.

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,2 +1,14 @@
 class GroupController < ApplicationController
+  def index
+    @groups = Group.all
+    if @groups.empty?
+      head :no_content
+    else
+      json_response(@groups)
+    end
+  end
+
+  def create
+
+  end
 end
--- spec/api/get_group_spec.rb
+++ spec/api/get_group_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
+require 'login_helper'
+require 'create_user_helper'
 
 RSpec.describe "Get all group routes", type: :request do
+  before(:each) do
+    create_user
+  end
   context "When a resource is found it..." do
     it "should return an http response (200)" do
+      FactoryBot.create(:group)
+      login
+      auth_params = get_auth_params(response)
+      get "/group", headers: auth_params
       expect(response).to have_http_status(200)
     end
   end
 
   context "When a resource is not found it..." do
     it "should return an http response (204)" do
+      login
+      auth_params = get_auth_params(response)
+      get "/group", headers: auth_params
       expect(response).to have_http_status(204)
     end
   end
