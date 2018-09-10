f1b8ed1c2d3db2edc18c1f2d83bbcad6dd41e5c6
Hunter <hunter.thomas.parks@gmail.com>
2018-09-09 17:18:10 -0700

    add tests for group validations

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -7,7 +7,11 @@ RSpec.describe "Group Model", type: :model do
   end
 
   context "WHen a new group is created it..." do
-    it "is valid with valid attributes"
-    it "is not valid without a title"
+    it "is valid with valid attributes" do
+      expect(Group.new(name: "My group", description: "This is my group")).to be_valid
+    end
+    it "is not valid without a title" do
+      expect(Group.new(name: nil, description: "This group doesn't have a name")).to_not be_valid
+    end
   end
 end
